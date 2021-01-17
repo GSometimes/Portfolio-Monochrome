fe780036cbbf0cbca7a6949cdc113d51a30e71cc
Gourav <gore.auluck@gmail.com>
2021-01-16 23:22:48 -0800

    Added Collage and Image Folder

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,16 @@ const About = () => {
             <NavBar />
             <div className="aboutContainer">
                 <div className="aboutMe">
-                    <h1>About</h1>
+                    <p className="aboutTitle">About</p>
                 </div>
+                <div className="imageGrid">
+                    <div className="image1"></div>
+                    <div className="image2"></div>
+                    <div className="image3"></div>
+                    <div className="image4"></div>
+                    <div className="image5"></div>
+                </div>
+
                 
             </div>
             

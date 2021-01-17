df44f22d0335cbb93ae82fa9314ba53741a90bce
Gourav <gore.auluck@gmail.com>
2021-01-17 14:30:04 -0800

    Added Media to Work/About Components

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,7 +7,29 @@ const Work = () => {
         <>
             <NavBar />
             <div className="workContainer">
-                
+            <div class="container">
+                <div class="box">
+                    <div class="imgBox">
+                        <img src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1610919748/Portfolio1/2021.01.17-13.32_qdtzcv.png" alt=""/>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBox">
+                        <img src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1607133056/Portfolio1/the_man_called_cayde_by_vulshokbersrker_dcxfgx8-fullview300_gly2rl.jpg" alt=""/>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBox">
+                        <img src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1610919751/Portfolio1/2021.01.17-13.33_jrp0hj.png" alt=""/>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBox">
+                        <img src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1607133621/Portfolio1/Screenshot_2020-11-29_222957_hjtdqz.png" alt=""/>
+                    </div>
+                </div>
+        </div>
+    
             </div>
 
         </>

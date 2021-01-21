c5e79945ca17dc7824cd353dbbbe508609f90219
Gourav <gore.auluck@gmail.com>
2021-01-21 00:05:56 -0800

    Updated Styling/Responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ const About = () => {
                 </div>
                 <div className="aboutParagraph">
                     <img src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1611122961/Portfolio1/GS_Logo2_abyhtt.png" alt="GoreSometimes"/>
-                    <p>Hi, my name is Gourav Auluck but many know me as Gore Sometimes. I am a Chef turned Developer who’s looking to take his creativity from plate to page and is excited to explore this new world of design and development. I take simple ideas and turn them into modern design.</p>
+                    <p>Hi, my name is Gourav Auluck but many know me as Gore Sometimes. I am a Chef turned Developer who’s looking to take my creativity from plate to page and is excited to explore this new world of design and development. I take simple ideas and turn them into modern design.</p>
                     <p>I am a father, foodie, and avid gamer always looking to make lasting memories. My son is my “why” and the reason I do what I do. When I am not online you’ll find me spending time with my son, enjoying a glass of wine with friends or grinding my way through the Destiny Universe.</p>
                     <p>I am a web developer based in Southern California looking for freelance and long-term opportunities. If you are a business seeking a web developer or an employer looking to hire you can get in touch with me <Link to="/contact">here</Link>.</p>
                     <img src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1611122961/Portfolio1/GS_Logo2_abyhtt.png" alt="GoreSometimes"/>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
     function sendEmail(e) {
         e.preventDefault();
     
-        emailjs.sendForm('gmail', 'template_gxyynmw', e.target, 'user_w1D9hYy51X1NfLEVhL0E9')
+        emailjs.sendForm('service_z49gvik', 'template_gxyynmw', e.target, 'user_w1D9hYy51X1NfLEVhL0E9')
           .then((result) => {
               console.log(result.text);
           }, (error) => {
@@ -24,20 +24,27 @@ const Contact = () => {
             <div className="contactContainer">
                 <p className="contactTitle">Contact</p>
                 <div className="contact-details">
-                    <h1><a href="mailto:gsometimes@outlook.com" target="_blank" rel="noopener noreferrer">GSometimes@outlook.com</a></h1>
+                    <p>Email</p>
+                    <h1><a href="mailto:gsometimes@outlook.com" target="_blank" rel="noopener noreferrer">gsometimes@outlook.com</a></h1>
+                    <br/>
+                    <br/>
+                    <p>Social</p>
                     <h1><a href="https://www.linkedin.com/in/gore-sometimes/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h1>
                     <h1><a href="https://github.com/GSometimes" target="_blank" rel="noopener noreferrer">Github</a></h1>
+                    <h1><a href="https://dev.to/gsometimes" target="_blank" rel="noopener noreferrer">Dev.to</a></h1>
+                    <p>Resume</p>
+                    <h1><a href="/currentResume.pdf" download>My Resume</a></h1>
                 </div>
                 <form className="contact-form" onSubmit={sendEmail}>
                     <h1>Let's Work Together!</h1>
                     <input type="hidden" name="contact_number" />
                     <label>Name</label>
-                    <input type="text" name="user_name" />
+                    <input type="text" name="name" required/>
                     <label>Email</label>
-                    <input type="email" name="user_email" />
+                    <input type="email" name="email" required/>
                     <label>Message</label>
-                    <textarea name="message" />
-                    <input type="submit" value="Send" />
+                    <textarea name="message" required/>
+                    <input id="button" type="submit" value="Send" />
                 </form>
 
             </div>

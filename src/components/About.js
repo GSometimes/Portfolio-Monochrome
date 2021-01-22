01dba102c4eb0ca0abe1374414b072ddb27121f1
Gourav <gore.auluck@gmail.com>
2021-01-21 17:12:12 -0800

    Mobile Responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ const About = () => {
                     <p>Hi, my name is Gourav Auluck but many know me as Gore Sometimes. I am a Chef turned Developer who’s looking to take my creativity from plate to page and is excited to explore this new world of design and development. I take simple ideas and turn them into modern design.</p>
                     <p>I am a father, foodie, and avid gamer always looking to make lasting memories. My son is my “why” and the reason I do what I do. When I am not online you’ll find me spending time with my son, enjoying a glass of wine with friends or grinding my way through the Destiny Universe.</p>
                     <p>I am a web developer based in Southern California looking for freelance and long-term opportunities. If you are a business seeking a web developer or an employer looking to hire you can get in touch with me <Link to="/contact"> here</Link>.</p>
-                    <img src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1611122961/Portfolio1/GS_Logo2_abyhtt.png" alt="GoreSometimes"/>
+                    <img src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1611277322/Portfolio1/Screenshot_2021-01-21_170120_omncrd.png" alt="GoreSometimes"/>
                 </div>
                 
             </div>

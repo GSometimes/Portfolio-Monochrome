93821d917d8b2fa9e0f3a12908d48b07b2cf8e4a
Gourav <gore.auluck@gmail.com>
2021-01-21 22:40:39 -0800

    Updated Responsiveness Mobile

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,7 @@ import '../App.css';
 import { Link } from "react-router-dom";
 import { init } from "ityped";
 
-// const Landing = () => {
+
   export default class Landing extends Component {
 
   componentDidMount() {
@@ -16,13 +16,7 @@ import { init } from "ityped";
 
     render () { return (
         <>
-          {/* <div className="mobileBanner">
-              <h1>PORT</h1>
-              <h1>FOLI</h1>
-              <h1>OBYS</h1>
-              <h1>OMET</h1>
-              <h1>IMES</h1>
-          </div> */}
+
           <div className="myName">
                 <img id ="myPic" src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1607283143/Portfolio1/829ABC3F-387C-44D9-978F-4D8D702D4865300_uttnwf.jpg" alt=""/>
                 <p id="hello">Hello, my name is Gore Sometimes. I'm a SoCal based <span id="myElement"></span> Chef, and Dad. <br/><br/>I take simple ideas and turn them into modern design.</p> 
@@ -36,4 +30,3 @@ import { init } from "ityped";
 }
 }
 
-// export default Landing;

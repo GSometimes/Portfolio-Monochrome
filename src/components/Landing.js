5dea68f69c2118208e21d3de3f8822505ff795ba
Gourav <gore.auluck@gmail.com>
2021-03-03 11:54:42 -0800

    Minor style updates

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -19,11 +19,11 @@ import Fade from 'react-reveal/Fade';
     render () { return (
         <>
           <div className="myName">
-          <Fade top>
-                <img id ="myPic" src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1607283143/Portfolio1/829ABC3F-387C-44D9-978F-4D8D702D4865300_uttnwf.jpg" alt=""/>
-                <p id="hello">Hello, my name is Gore Sometimes. I'm a SoCal based <span id="myElement"></span> Chef, and Dad. <br/><br/>I take simple ideas and turn them into modern design.</p> 
-          </Fade>
-            </div>
+            <Fade top>
+                  <img id ="myPic" src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1607283143/Portfolio1/829ABC3F-387C-44D9-978F-4D8D702D4865300_uttnwf.jpg" alt=""/>
+                  <p id="hello">Hello, my name is Gore Sometimes. I'm a SoCal based <span id="myElement"></span> Chef, and Dad. <br/><br/>I take simple ideas and turn them into modern design.</p> 
+            </Fade>
+          </div>
 
           <Fade bottom>
           <div className="landingContainer">
